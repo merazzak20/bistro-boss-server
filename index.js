c048bc6355759a5bf56ed8625bfe0b008058c5dd
merazzak <info.merazzak@gmail.com>
2025-01-04 02:23:40 +0600

    3rd

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     const db = client.db("bistroDb");
     const menuCollection = db.collection("menu");
     const reviewsCollection = db.collection("reviews");
+    const cartsCollection = db.collection("carts");
 
     // get all menu
     app.get("/menu", async (req, res) => {
@@ -37,6 +38,19 @@ async function run() {
     // get all reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Cart Related API
+
+    app.get("/carts", async (req, res) => {
+      const result = await cartsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
       res.send(result);
     });
 
